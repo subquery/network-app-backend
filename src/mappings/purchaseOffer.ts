f7b06ee2f7b1fd4485411f2fdab0bfd875162b85
CaiYiLiang <cherryliang0112@gmail.com>
2022-06-03 16:57:59 +1200

    feat: add offerMarketLogic

---
+++ src/mappings/purchaseOffer.ts
@@ -0,0 +1,60 @@
+// Copyright 2020-2022 SubQuery Pte Ltd authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import assert from 'assert';
+import {
+  PurchaseOfferCreatedEvent,
+  PurchaseOfferCancelledEvent,
+} from '@subql/contract-sdk/typechain/PurchaseOfferMarket';
+import { PurchaseOfferMarket__factory } from '@subql/contract-sdk';
+import { AcalaEvmEvent } from '@subql/acala-evm-processor';
+import FrontierEthProvider from './ethProvider';
+import { Offer } from '../types';
+import { bytesToIpfsCid } from './utils';
+
+// TODO: to confirm expireDate -  should be Date type
+// TODO: to confirm offerID - should expose from event args
+export async function handlePurchaseOfferCreated(
+  event: AcalaEvmEvent<PurchaseOfferCreatedEvent['args']>
+): Promise<void> {
+  logger.info('handlePurchaseOfferCreated');
+  assert(event.args, 'No event args');
+
+  const offerContract = PurchaseOfferMarket__factory.connect(
+    event.args.consumer,
+    new FrontierEthProvider()
+  );
+  const offerId = await offerContract.numOffers();
+
+  const offer = Offer.create({
+    id: offerId.toString(),
+    consumer: event.args.consumer,
+    deploymentId: bytesToIpfsCid(event.args.deploymentId),
+    planTemplateId: event.args.planTemplateId.toHexString(),
+    deposit: event.args.deposit.toBigInt(),
+    minimumAcceptHeight: event.args.minimumAcceptHeight.toBigInt(),
+    expireDate: new Date(event.args.expireDate.toString()),
+    limit: event.args.limit,
+    accepted: 0,
+    reachLimit: false,
+    withdrawn: false,
+  });
+
+  await offer.save();
+}
+
+export async function handlePurchaseOfferCancelled(
+  event: AcalaEvmEvent<PurchaseOfferCancelledEvent['args']>
+): Promise<void> {
+  logger.info('handlePurchaseOfferCancelled');
+  assert(event.args, 'No event args');
+
+  const offer = await Offer.get(event.args.offerId.toString());
+  assert(offer, `offer not found. planId="${event.args.offerId.toString()}"`);
+
+  offer.expireDate = new Date(event.blockTimestamp);
+  offer.withdrawn = true;
+  offer.withdrawPenalty = event.args.penalty.toBigInt();
+
+  await offer.save();
+}
