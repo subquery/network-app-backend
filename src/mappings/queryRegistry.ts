4ebb7aed2e4616ec715a6199d4f5951dbc3df826
CaiYiLiang <cherryliang0112@gmail.com>
2022-07-08 13:11:58 +1200

    feat: fix handleIndexingReady

--- src/mappings/queryRegistry.ts
+++ src/mappings/queryRegistry.ts
@@ -3,7 +3,7 @@
 
 import assert from 'assert';
 import { DeploymentIndexer, Deployment, Project, Status } from '../types';
-
+import { QueryRegistry__factory } from '@subql/contract-sdk';
 import {
   CreateQueryEvent,
   StartIndexingEvent,
@@ -13,11 +13,73 @@ import {
   UpdateQueryDeploymentEvent,
   UpdateIndexingStatusToReadyEvent,
 } from '@subql/contract-sdk/typechain/QueryRegistry';
-import { bnToDate, bytesToIpfsCid } from './utils';
+import {
+  bnToDate,
+  bytesToIpfsCid,
+  cidToBytes32,
+  QUERY_REGISTRY_ADDRESS,
+} from './utils';
 import { AcalaEvmEvent } from '@subql/acala-evm-processor';
+import FrontierEthProvider from './ethProvider';
 
 function getDeploymentIndexerId(indexer: string, deploymentId: string): string {
   return `${indexer}:${deploymentId}`;
+}
+
+interface ISaveDeploymentIndexer {
+  indexerId: string;
+  deploymentId: string;
+  blockHeight?: bigint;
+  timestamp?: Date;
+  mmrRoot?: string;
+  status: Status;
+}
+
+async function createDeploymentIndexer({
+  indexerId,
+  deploymentId,
+  blockHeight,
+  timestamp,
+  mmrRoot,
+  status,
+}: ISaveDeploymentIndexer) {
+  logger.info(`createDeploymentIndexer: ${indexerId}`);
+  const sortedBlockHeight = blockHeight || BigInt(0);
+
+  // try {
+  //   if (!blockHeight) {
+  //     const queryRegistryManager = QueryRegistry__factory.connect(
+  //       QUERY_REGISTRY_ADDRESS,
+  //       new FrontierEthProvider()
+  //     );
+
+  //     const deploymentStatus =
+  //       await queryRegistryManager.deploymentStatusByIndexer(
+  //         cidToBytes32(deploymentId),
+  //         indexerId
+  //       );
+
+  //     logger.info(
+  //       `====== deploymentStatus: ${deploymentStatus?.blockHeight.toBigInt()}`
+  //     );
+
+  //     sortedBlockHeight = deploymentStatus.blockHeight.toBigInt();
+  //   }
+  // } catch (error) {
+  //   logger.error(error);
+  //   throw Error(error);
+  // }
+
+  const indexer = DeploymentIndexer.create({
+    id: getDeploymentIndexerId(indexerId, deploymentId),
+    indexerId,
+    deploymentId: deploymentId,
+    blockHeight: sortedBlockHeight,
+    timestamp,
+    mmrRoot,
+    status,
+  });
+  await indexer.save();
 }
 
 export async function handleNewQuery(
@@ -113,22 +175,40 @@ export async function handleStartIndexing(
   await indexer.save();
 }
 
+/**
+ * NOTE: 8Jul 2022
+ * Event order: handleIndexingReady -> handleIndexingUpdate
+ */
 export async function handleIndexingUpdate(
   event: AcalaEvmEvent<UpdateDeploymentStatusEvent['args']>
 ): Promise<void> {
-  // logger.info('handleIndexingUpdate');
+  logger.info('handleIndexingUpdate');
   assert(event.args, 'No event args');
   const deploymentId = bytesToIpfsCid(event.args.deploymentId);
   const id = getDeploymentIndexerId(event.args.indexer, deploymentId);
   const indexer = await DeploymentIndexer.get(id);
 
-  assert(indexer, `Expected deployment indexer (${id}) to exist`);
-  indexer.blockHeight = event.args.blockheight.toBigInt();
-  indexer.mmrRoot = event.args.mmrRoot;
-  indexer.timestamp = bnToDate(event.args.timestamp);
-  await indexer.save();
+  if (!indexer) {
+    await createDeploymentIndexer({
+      indexerId: id,
+      deploymentId,
+      blockHeight: event.args.blockheight.toBigInt(),
+      mmrRoot: event.args.mmrRoot,
+      timestamp: event.blockTimestamp,
+      status: Status.READY,
+    });
+  } else {
+    indexer.blockHeight = event.args.blockheight.toBigInt();
+    indexer.mmrRoot = event.args.mmrRoot;
+    indexer.timestamp = bnToDate(event.args.timestamp);
+    await indexer.save();
+  }
 }
 
+/**
+ * NOTE: 8Jul 2022
+ * Event order: handleStartIndexing -> handleIndexingReady
+ */
 export async function handleIndexingReady(
   event: AcalaEvmEvent<UpdateIndexingStatusToReadyEvent['args']>
 ): Promise<void> {
@@ -138,10 +218,18 @@ export async function handleIndexingReady(
   const id = getDeploymentIndexerId(event.args.indexer, deploymentId);
   const indexer = await DeploymentIndexer.get(id);
 
-  assert(indexer, `Expected deployment indexer (${id}) to exist`);
-  indexer.status = Status.READY;
-  indexer.timestamp = event.blockTimestamp;
-  await indexer.save();
+  if (!indexer) {
+    await createDeploymentIndexer({
+      indexerId: id,
+      deploymentId,
+      timestamp: event.blockTimestamp,
+      status: Status.READY,
+    });
+  } else {
+    indexer.status = Status.READY;
+    indexer.timestamp = event.blockTimestamp;
+    await indexer.save();
+  }
 }
 
 export async function handleStopIndexing(
