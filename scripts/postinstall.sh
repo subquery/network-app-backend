a160288216492d7c2376b3d95165b63ada29ecb5
HZ <zhaihaohk@gmail.com>
2022-08-25 11:05:19 +1200

    Fix postinstall script

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,3 +1,8 @@
 # FIXME: this is a hack code to resolve the build issue for using `base64` module
 sed -i -e "s/\"module\".*/\"module\": \".\/lib\/index.js\",/g" ./node_modules/@ethersproject/base64/package.json
-rm ./node_modules/@ethersproject/base64/package.json-e
+
+ERROR_FILE="./node_modules/@ethersproject/base64/package.json-e"
+
+if [ -f $ERROR_FILE ] ; then
+    rm $ERROR_FILE
+fi
