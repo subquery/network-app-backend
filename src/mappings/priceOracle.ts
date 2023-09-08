1a7bf5eacacb6168c6e734b3a5cfe0abd80d2e4e
mzxyz <8177474+mzxyz@users.noreply.github.com>
2023-09-08 14:12:51 +1200

    Feat: stable coin (#91)
    * Upgrade contract SDK to `v0.16.0`
    
    * Add `priceOracleHandler`
    
    * Update testnet cid
    
    * fix test yml file

---
+++ src/mappings/priceOracle.ts
@@ -0,0 +1,29 @@
+// Copyright 2020-2022 SubQuery Pte Ltd authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { PricePostedEvent } from '@subql/contract-sdk/typechain/PriceOracle';
+import { EthereumLog } from '@subql/types-ethereum';
+import assert from 'assert';
+import { PriceOracle } from '../types';
+
+export async function handlePricePosted(
+  event: EthereumLog<PricePostedEvent['args']>
+): Promise<void> {
+  logger.info('handlePricePosted');
+  assert(event.args, 'No event args');
+
+  const { assetFrom, assetTo, previousPrice, newPrice } = event.args;
+
+  const blockNum = event.blockNumber;
+
+  const price = PriceOracle.create({
+    id: `${assetFrom}-${assetTo}-${blockNum}`,
+    fromToken: assetFrom,
+    toToken: assetTo,
+    beforePrice: previousPrice.toBigInt(),
+    afterPrice: newPrice.toBigInt(),
+    createdBlock: blockNum,
+  });
+
+  await price.save();
+}
