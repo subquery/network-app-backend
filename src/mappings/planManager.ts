f88186e3a8ae9d82643152b96051319acaa3b50e
mzxyz <8177474+mzxyz@users.noreply.github.com>
2023-09-12 09:41:42 +1200

    Add diff version of contract-sdk

--- src/mappings/planManager.ts
+++ src/mappings/planManager.ts
@@ -9,43 +9,77 @@ import {
   PlanTemplateStatusChangedEvent,
 } from '@subql/contract-sdk/typechain/PlanManager';
 
+import { PlanManager__factory as PlanManager__factory_V2 } from '@subql/contract-sdk';
+import { PlanManager__factory as PlanManager__factory_V1 } from '@subql/contract-sdk-15';
+import { PlanTemplateStructOutput } from '@subql/contract-sdk-15/typechain/IPlanManager';
+import { PlanTemplateV2Struct } from '@subql/contract-sdk/typechain/IPlanManager';
 import { EthereumLog } from '@subql/types-ethereum';
 import assert from 'assert';
-import { constants } from 'ethers';
+import { BigNumber, constants } from 'ethers';
 import { Plan, PlanTemplate } from '../types';
-import { PlanManager__factory } from '../types/contracts/factories/PlanManager__factory';
 import { Contracts, bytesToIpfsCid, getContractAddress } from './utils';
+
+const blockNumberV2: Record<string, number> = {
+  '80001': 40021190,
+  '137': 47430654,
+};
+
+function planTemplateV1ToV2(
+  tempalte: PlanTemplateStructOutput,
+  chainId: number
+): PlanTemplateV2Struct {
+  const sqtTokenAddress = getContractAddress(chainId, Contracts.KSQT_ADDRESS);
+  return {
+    period: tempalte.period,
+    dailyReqCap: tempalte.dailyReqCap,
+    rateLimit: tempalte.rateLimit,
+    priceToken: sqtTokenAddress,
+    metadata: tempalte.metadata,
+    active: tempalte.active,
+  };
+}
 
 export async function handlePlanTemplateCreated(
   event: EthereumLog<PlanTemplateCreatedEvent['args']>
 ): Promise<void> {
-  // TODO: handle testnet / kepler difference
-  if (event.blockNumber > 1000) {
-    return;
-  }
   logger.info('handlePlanTemplateCreated');
   assert(event.args, 'No event args');
 
   const network = await api.getNetwork();
-  const planManager = PlanManager__factory.connect(
-    getContractAddress(network.chainId, Contracts.PLAN_MANAGER_ADDRESS),
-    api
+
+  let rawPlanTemplate: PlanTemplateV2Struct;
+  const planManagerAddress = getContractAddress(
+    network.chainId,
+    Contracts.PLAN_MANAGER_ADDRESS
   );
 
-  const rawPlanTemplate = await planManager.getPlanTemplate(
-    event.args.templateId
-  );
+  if (event.blockNumber < blockNumberV2[network.chainId]) {
+    const planManager = PlanManager__factory_V1.connect(
+      planManagerAddress,
+      api
+    );
+    const rawPlanTemplateV1 = await planManager.getPlanTemplate(
+      event.args.templateId
+    );
+    rawPlanTemplate = planTemplateV1ToV2(rawPlanTemplateV1, network.chainId);
+  } else {
+    const planManager = PlanManager__factory_V2.connect(
+      planManagerAddress,
+      api
+    );
+    rawPlanTemplate = await planManager.getPlanTemplate(event.args.templateId);
+  }
 
   const planTemplate = PlanTemplate.create({
     id: event.args.templateId.toHexString(),
-    period: rawPlanTemplate.period.toBigInt(),
-    dailyReqCap: rawPlanTemplate.dailyReqCap.toBigInt(),
-    rateLimit: rawPlanTemplate.rateLimit.toBigInt(),
+    period: BigNumber.from(rawPlanTemplate.period).toBigInt(),
+    dailyReqCap: BigNumber.from(rawPlanTemplate.dailyReqCap).toBigInt(),
+    rateLimit: BigNumber.from(rawPlanTemplate.rateLimit).toBigInt(),
     priceToken: rawPlanTemplate.priceToken,
     metadata:
       constants.HashZero === rawPlanTemplate.metadata
         ? undefined
-        : bytesToIpfsCid(rawPlanTemplate.metadata),
+        : bytesToIpfsCid(rawPlanTemplate.metadata.toString()),
     active: true,
     createdBlock: event.blockNumber,
   });
