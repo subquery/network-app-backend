522129c1ab0d9dfd7181318200483fca1c27652f
Jacob <princewith@gmail.com>
2024-01-29 18:34:47 +0800

    remove: trader and trade

--- src/mappings/tokenExchange.ts
+++ src/mappings/tokenExchange.ts
@@ -8,7 +8,7 @@ import {
 } from '@subql/contract-sdk/typechain/contracts/TokenExchange';
 import { EthereumLog } from '@subql/types-ethereum';
 import assert from 'assert';
-import { Order, OrderStatus, Trade, Trader } from '../types';
+import { Order, OrderStatus } from '../types';
 import { biToDate } from './utils';
 
 export async function handleExchangeOrderSent(
@@ -58,44 +58,4 @@ export async function handleOrderSettled(
   order.updateAt = biToDate(event.block.timestamp);
 
   await order.save();
-}
-
-export async function handleTraded(
-  event: EthereumLog<TradeEvent['args']>
-): Promise<void> {
-  logger.info('handleTraded');
-  assert(event.args, 'No event args');
-
-  const { orderId, tokenGive, tokenGet, amountGive, amountGet } = event.args;
-
-  const sender = event.transaction.from;
-  let trader = await Trader.get(sender);
-  if (!trader) {
-    trader = Trader.create({
-      id: sender,
-      totalSwapSQTAmount: amountGive.toBigInt(),
-      totalSwapkSQTAmount: amountGet.toBigInt(),
-      createAt: biToDate(event.block.timestamp),
-      updateAt: biToDate(event.block.timestamp),
-    });
-  } else {
-    trader.totalSwapSQTAmount += amountGive.toBigInt();
-    trader.totalSwapkSQTAmount += amountGet.toBigInt();
-    trader.updateAt = biToDate(event.block.timestamp);
-  }
-
-  await trader.save();
-
-  const tradeRecord = Trade.create({
-    id: `${orderId.toString()}:${event.transaction.hash}`,
-    senderId: sender,
-    tokenGive,
-    tokenGet,
-    amountGive: amountGive.toBigInt(),
-    amountGet: amountGet.toBigInt(),
-    createAt: biToDate(event.block.timestamp),
-    updateAt: biToDate(event.block.timestamp),
-  });
-
-  await tradeRecord.save();
 }
